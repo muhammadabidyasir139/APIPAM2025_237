dc3490b989f7e4e27fa6d35ad648db540c5c014e
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2025-12-26 21:43:18 +0700

    fix s3 bucket name for update

--- src/config/s3.js
+++ src/config/s3.js
@@ -1,12 +1,15 @@
 const AWS = require("aws-sdk");
 
-const s3 = new AWS.S3({
+AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION || "us-east-1",
+});
+
+const s3 = new AWS.S3({
   endpoint: process.env.AWS_S3_ENDPOINT,
   s3BucketEndpoint: true,
   s3ForcePathStyle: true,
-  region: process.env.AWS_REGION || "us-east-1",
 });
 
 const bucketName = process.env.AWS_S3_BUCKET_NAME;

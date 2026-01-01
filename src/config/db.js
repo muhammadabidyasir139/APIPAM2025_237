6c1914c1bbdb6c8c75c8d7653b8046ded43c4a42
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2026-01-01 10:32:04 +0700

    add revenue for admin

--- src/config/db.js
+++ src/config/db.js
@@ -6,10 +6,9 @@ const db = new Pool({
     `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${
       process.env.DB_HOST
     }:${process.env.DB_PORT || 5432}/${process.env.DB_NAME}`,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 db.connect((err, client, release) => {
